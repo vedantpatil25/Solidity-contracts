1eac2423c9f7ffe9a58d12a5bddf495360dcc4c8
Vedant Patil <106603826+vedantpatil25@users.noreply.github.com>
2023-01-30 23:03:44 +0530

    Update 2_Variables.sol

--- examples/2_Variables.sol
+++ examples/2_Variables.sol
@@ -1,27 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+//By Vedant
+
+
 contract Variables {
-    // State Variables
-    int256 public myInt = 1;
+
+
+    int256 public myInt = 69;
     uint256 public myUint256 = 1;
+
+
+    uint public myUint = 36;
     uint8 public myUint8 = 1;
 
     string public myString = "Hello, World!";
+
     bytes32 public myBytes32 = "Hello, World!";
+    //treats string like array
 
     address public myAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    //works like username
 
+
+
+    //custom dataType
     struct MyStruct {
-        uint256 myUint256;
+        uint myUint;
         string myString;
     }
 
-    MyStruct public myStruct = MyStruct(1, "Hello, World!");
+    MyStruct public myStruct = MyStruct(1, "Hello Vedant");
 
-    // Local Variables
+
+    //local Variables: scope in fuction only
     function getValue() public pure returns (uint256) {
-        uint256 value = 1;
+        uint value = 1;
         return value;
     }
 }
