3a5f8b28302b577ea30b9f62a133fb924f6cda5b
Vedant Patil <106603826+vedantpatil25@users.noreply.github.com>
2023-02-06 09:23:12 +0530

    Update and rename 2_Variables.sol to Variables.sol

---
+++ examples/Variables.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Variables {
+
+    int256 public myInt = 69;
+    uint256 public myUint256 = 1;
+
+    uint public myUint = 36;
+    uint8 public myUint8 = 1;
+
+    string public myString = "Hello, Vedant!";
+
+    bytes32 public myBytes32 = "Hello, Vedant!";
+    //treats string like array
+
+    address public myAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    //works like username
+
+
+
+    //custom dataType stuct
+    struct MyStruct {
+        uint myUint;
+        string myString;
+    }
+
+    MyStruct public myStruct = MyStruct(1, "Hello Vedant");
+
+
+    //local Variables : scope in fuction only
+    function getValue() public pure returns (uint256) {
+        uint value = 1;
+        return value;
+    }
+}
