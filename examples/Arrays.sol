4b55d27b2838251ca079efe724b8f8fc649376f0
Vedant Patil <106603826+vedantpatil25@users.noreply.github.com>
2023-02-06 09:25:56 +0530

    Update and rename 3_Arrays.sol to Arrays.sol

--- examples/Arrays.sol
+++ examples/Arrays.sol
@@ -2,15 +2,22 @@
 pragma solidity ^0.8.0;
 
 contract Arrays {
-    uint256[] public uint256Array = [1, 2, 3];
-    string[] public stringArray = ["apple", "banana", "carrot"];
+    uint256[] public tempArr = [235,23,5,2435,2,5,45,2,5,2343,4];
+
+    string[] public stringArray = ["sdgds", "dsfgs", "agfaf", "gfdadfg"];
     string[] public values;
+
+
     uint256[][] public array2D = [[1, 2, 3], [4, 5, 6]];
 
+
+    //********* memory is data location ------>
     function addValue(string memory _value) public {
         values.push(_value);
     }
 
+
+    //size of array
     function valueCount() public view returns (uint256) {
         return values.length;
     }
